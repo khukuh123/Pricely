c70ec385db026c9a629e98335eb825045e50dc3e
khukuh123 <khukuh.prihatmikho2@gmail.com>
2022-05-22 00:12:34 +0700

    style(splash): create splash screen with API

--- app/src/main/java/com/bangkit/pricely/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/pricely/presentation/main/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.bangkit.pricely.presentation.main
 
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bangkit.pricely.base.BaseActivity
 import com.bangkit.pricely.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-    override fun getViewBinding(): ActivityMainBinding =
-        ActivityMainBinding.inflate(layoutInflater)
 
-    override fun setupIntent() {
-
+    override fun getViewBinding(): ActivityMainBinding {
+        installSplashScreen()
+        return ActivityMainBinding.inflate(layoutInflater)
     }
 
-    override fun setupUI() {
+    override fun setupIntent() {}
 
-    }
+    override fun setupUI() {}
 
-    override fun setupAction() {
+    override fun setupAction() {}
 
-    }
+    override fun setupProcess() {}
 
-    override fun setupProcess() {
-
-    }
-
-    override fun setupObserver() {
-
-    }
+    override fun setupObserver() {}
 }
